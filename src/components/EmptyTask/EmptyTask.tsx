02bbfaaa1c1ee4aa8b4a3c60bda3d7e77010ff9d
Raimones Barros <raimonesrg3@gmail.com>
2023-06-12 21:33:59 -0300

    fix: change src img

--- src/components/EmptyTask/EmptyTask.tsx
+++ src/components/EmptyTask/EmptyTask.tsx
@@ -1,9 +1,10 @@
 import styles from "./EmptyTask.module.css";
+//import clipboard from '../../assets/Clipboard.svg'
 
 export function EmptyTask() {
   return (
     <div className={styles.empty}>
-      <img src="./src/assets/Clipboard.svg" alt="clipboard" />
+      <img src='../../assets/Clipboard.svg' alt="" />
       <p>
         Você ainda não tem tarefas cadastradas
         <span>Crie tarefas e organize seus itens a fazer</span>
